8dc6a46966000182a40e427308dd79d9c5a6cd96
Андрей Ефимов <efiand@ya.ru>
2020-12-20 16:10:51 +0300

    0.1.4 add standard gulp config

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 'use strict';
 
+const codeguide = {
+	eslint: require('eslint-config-pineglade'),
+	stylelint: require('./stylelint'),
+	htmlhint: require('./htmlhint')
+};
+
+const config = {
+	htmlmin: require('./htmlmin'),
+	svgo: require('./svgo')
+};
+
 module.exports = {
-	codeguide: {
-		eslint: require('eslint-config-pineglade'),
-		stylelint: require('./stylelint'),
-		htmlhint: require('./htmlhint')
-	},
-	config: {
-		htmlmin: require('./htmlmin'),
-		svgo: require('./svgo')
+	codeguide, // for compatibility
+	config, // for compatibility
+	gulp: {
+		DEST: 'build',
+		ESLINT: {
+			fix: false
+		},
+		HTMLHINT: codeguide.htmlhint,
+		HTMLMIN: config.htmlmin,
+		JPEGOPTIM: {
+			max: 80,
+			progressive: true
+		},
+		LINTSPACES: {
+			editorconfig: '.editorconfig'
+		},
+		SERVER: {
+			cors: true,
+			notify: false,
+			ui: false
+		},
+		STYLELINT: {
+			reporters: [
+				{
+					console: true,
+					formatter: 'string'
+				}
+			]
+		},
+		SVGO: config.svgo,
+		SVGSTORE: {
+			inlineSvg: true
+		},
+		WEBP: {
+			quality: 80
+		},
+		WEBPACK: {
+			mode: 'production',
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						use: {
+							loader: 'babel-loader',
+							options: {
+								presets: [
+									[
+										'@babel/preset-env'
+									]
+								]
+							}
+						}
+					}
+				]
+			},
+			optimization: {
+				minimize: true
+			}
+		}
 	}
 };
